1033ee0850cd20ac702f566a1ffcea8a5075d394
trkoding <trkoding@gmail.com>
2023-07-27 17:40:00 +0800

    first update

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+
+  -- { "folke/tokyonight.nvim" },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      colorscheme = "tokyonight",
+      style = "night",
+    },
+  },
+}
