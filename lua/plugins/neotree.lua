af7b8a879d6f6a4c7bb8ad0822a760f3eb078aa2
trkoding <trkoding@gmail.com>
2023-07-31 13:19:29 +0800

    show dotfiles

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          always_show = {
+            ".gitignore",
+            ".env.example",
+            ".env",
+          },
+        },
+      },
+    },
+  },
+}
