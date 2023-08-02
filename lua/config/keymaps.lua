774e65180ba1d9b7ef48ecc3675240fb21dbeed2
trkoding <131626791+trkoding@users.noreply.github.com>
2023-08-02 12:08:40 +0800

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+del = vim.keymap.del
+-- GOTO
+del("n", "<C-h>")
+del("n", "<C-j>")
+del("n", "<C-k>")
+del("n", "<C-l>")
+del("t", "<C-h>")
+del("t", "<C-j>")
+del("t", "<C-k>")
+del("t", "<C-l>")
+
+-- Increase/Decrease
+del("n", "<C-Up>")
+del("n", "<C-Down>")
+del("n", "<C-Left>")
+del("n", "<C-Right>")
+
+-- Terminal
+del("n", "<c-/>")
+del("n", "<C-/>")
